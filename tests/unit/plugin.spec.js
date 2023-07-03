2429463775b4999661a422c7477bb9cb36709b0d
Cal Courtney <cal@absmartly.com>
2023-07-03 14:54:03 +0100

    Removed conflicting prettier rules from eslintrc

--- tests/unit/plugin.spec.js
+++ tests/unit/plugin.spec.js
@@ -6,295 +6,295 @@ import Treatment from "@/components/Treatment.vue";
 jest.mock("@absmartly/javascript-sdk");
 
 const mockCreateContext = jest.fn().mockImplementation(() => {
-    return new Context();
+	return new Context();
 });
 
 const mockCreateContextWith = jest.fn().mockImplementation(() => {
-    return new Context();
+	return new Context();
 });
 
 SDK.mockImplementation(() => {
-    return {
-        createContext: mockCreateContext,
-        createContextWith: mockCreateContextWith,
-    };
+	return {
+		createContext: mockCreateContext,
+		createContextWith: mockCreateContextWith,
+	};
 });
 
 describe("ABSmartly Vue.js Plugin", () => {
-    const Component = {
-        template: "<div>test</div>",
-    };
+	const Component = {
+		template: "<div>test</div>",
+	};
 
-    const sdkOptions = {
-        test: 1,
-    };
+	const sdkOptions = {
+		test: 1,
+	};
 
-    const contextOptions = {
-        refreshPeriod: 600000,
-        test: 2,
-    };
+	const contextOptions = {
+		refreshPeriod: 600000,
+		test: 2,
+	};
 
-    const context = {
-        test: 2,
-    };
+	const context = {
+		test: 2,
+	};
 
-    const data = {
-        test: 2,
-    };
+	const data = {
+		test: 2,
+	};
 
-    const attrs = {
-        attr1: "value1",
-        attr2: "value2",
-    };
+	const attrs = {
+		attr1: "value1",
+		attr2: "value2",
+	};
 
-    const overrides = {
-        not_found: 2,
-    };
+	const overrides = {
+		not_found: 2,
+	};
 
-    it("should create SDK and context", (done) => {
-        const wrapper = mount(Component, {
-            global: {
-                plugins: [
-                    [
-                        ABSmartly,
-                        {
-                            sdkOptions,
-                            context,
-                            contextOptions,
-                            attributes: attrs,
-                            overrides,
-                        },
-                    ],
-                ],
-            },
-        });
+	it("should create SDK and context", (done) => {
+		const wrapper = mount(Component, {
+			global: {
+				plugins: [
+					[
+						ABSmartly,
+						{
+							sdkOptions,
+							context,
+							contextOptions,
+							attributes: attrs,
+							overrides,
+						},
+					],
+				],
+			},
+		});
 
-        expect(SDK).toHaveBeenCalledTimes(1);
-        expect(SDK).toHaveBeenLastCalledWith(sdkOptions);
-        expect(mockCreateContext).toHaveBeenCalledTimes(1);
-        expect(mockCreateContext).toHaveBeenCalledWith(context, contextOptions);
+		expect(SDK).toHaveBeenCalledTimes(1);
+		expect(SDK).toHaveBeenLastCalledWith(sdkOptions);
+		expect(mockCreateContext).toHaveBeenCalledTimes(1);
+		expect(mockCreateContext).toHaveBeenCalledWith(context, contextOptions);
 
-        expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledTimes(1);
-        expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledWith(attrs);
+		expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledTimes(1);
+		expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledWith(attrs);
 
-        expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledTimes(1);
-        expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledWith(overrides);
+		expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledTimes(1);
+		expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledWith(overrides);
 
-        done();
-    });
-    it("should create context with default options", (done) => {
-        const wrapper = mount(Component, {
-            global: {
-                plugins: [
-                    [
-                        ABSmartly,
-                        {
-                            sdkOptions,
-                            context,
-                            attributes: attrs,
-                            overrides,
-                        },
-                    ],
-                ],
-            },
-        });
+		done();
+	});
+	it("should create context with default options", (done) => {
+		const wrapper = mount(Component, {
+			global: {
+				plugins: [
+					[
+						ABSmartly,
+						{
+							sdkOptions,
+							context,
+							attributes: attrs,
+							overrides,
+						},
+					],
+				],
+			},
+		});
 
-        expect(SDK).toHaveBeenCalledTimes(1);
-        expect(SDK).toHaveBeenLastCalledWith(sdkOptions);
-        expect(mockCreateContext).toHaveBeenCalledTimes(1);
-        expect(mockCreateContext).toHaveBeenCalledWith(context, {
-            refreshPeriod: 300000,
-        });
+		expect(SDK).toHaveBeenCalledTimes(1);
+		expect(SDK).toHaveBeenLastCalledWith(sdkOptions);
+		expect(mockCreateContext).toHaveBeenCalledTimes(1);
+		expect(mockCreateContext).toHaveBeenCalledWith(context, {
+			refreshPeriod: 300000,
+		});
 
-        expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledTimes(1);
-        expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledWith(attrs);
+		expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledTimes(1);
+		expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledWith(attrs);
 
-        expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledTimes(1);
-        expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledWith(overrides);
+		expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledTimes(1);
+		expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledWith(overrides);
 
-        done();
-    });
+		done();
+	});
 
-    it("should create SDK and context with no attributes and no overrides", (done) => {
-        const wrapper = mount(Component, {
-            global: {
-                plugins: [
-                    [
-                        ABSmartly,
-                        {
-                            sdkOptions,
-                            context,
-                            contextOptions,
-                        },
-                    ],
-                ],
-            },
-        });
+	it("should create SDK and context with no attributes and no overrides", (done) => {
+		const wrapper = mount(Component, {
+			global: {
+				plugins: [
+					[
+						ABSmartly,
+						{
+							sdkOptions,
+							context,
+							contextOptions,
+						},
+					],
+				],
+			},
+		});
 
-        expect(SDK).toHaveBeenCalledTimes(1);
-        expect(SDK).toHaveBeenLastCalledWith(sdkOptions);
-        expect(mockCreateContext).toHaveBeenCalledTimes(1);
-        expect(mockCreateContext).toHaveBeenCalledWith(context, contextOptions);
+		expect(SDK).toHaveBeenCalledTimes(1);
+		expect(SDK).toHaveBeenLastCalledWith(sdkOptions);
+		expect(mockCreateContext).toHaveBeenCalledTimes(1);
+		expect(mockCreateContext).toHaveBeenCalledWith(context, contextOptions);
 
-        expect(wrapper.vm.$absmartly.attributes).not.toHaveBeenCalled();
-        expect(wrapper.vm.$absmartly.overrides).not.toHaveBeenCalled();
+		expect(wrapper.vm.$absmartly.attributes).not.toHaveBeenCalled();
+		expect(wrapper.vm.$absmartly.overrides).not.toHaveBeenCalled();
 
-        done();
-    });
+		done();
+	});
 
-    it("should create SDK and context with data", (done) => {
-        const wrapper = mount(Component, {
-            global: {
-                plugins: [
-                    [
-                        ABSmartly,
-                        {
-                            sdkOptions,
-                            context,
-                            data,
-                            contextOptions,
-                            attributes: attrs,
-                            overrides,
-                        },
-                    ],
-                ],
-            },
-        });
+	it("should create SDK and context with data", (done) => {
+		const wrapper = mount(Component, {
+			global: {
+				plugins: [
+					[
+						ABSmartly,
+						{
+							sdkOptions,
+							context,
+							data,
+							contextOptions,
+							attributes: attrs,
+							overrides,
+						},
+					],
+				],
+			},
+		});
 
-        expect(SDK).toHaveBeenCalledTimes(1);
-        expect(SDK).toHaveBeenLastCalledWith(sdkOptions);
-        expect(mockCreateContextWith).toHaveBeenCalledTimes(1);
-        expect(mockCreateContextWith).toHaveBeenCalledWith(context, data, contextOptions);
+		expect(SDK).toHaveBeenCalledTimes(1);
+		expect(SDK).toHaveBeenLastCalledWith(sdkOptions);
+		expect(mockCreateContextWith).toHaveBeenCalledTimes(1);
+		expect(mockCreateContextWith).toHaveBeenCalledWith(context, data, contextOptions);
 
-        expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledTimes(1);
-        expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledWith(attrs);
+		expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledTimes(1);
+		expect(wrapper.vm.$absmartly.attributes).toHaveBeenCalledWith(attrs);
 
-        expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledTimes(1);
-        expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledWith(overrides);
+		expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledTimes(1);
+		expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledWith(overrides);
 
-        done();
-    });
+		done();
+	});
 
-    it("should use passed context", (done) => {
-        const mockContext = new Context();
-        const wrapper = mount(Component, {
-            global: {
-                plugins: [
-                    [
-                        ABSmartly,
-                        {
-                            context: mockContext,
-                            attributes: attrs,
-                            overrides,
-                        },
-                    ],
-                ],
-            },
-        });
+	it("should use passed context", (done) => {
+		const mockContext = new Context();
+		const wrapper = mount(Component, {
+			global: {
+				plugins: [
+					[
+						ABSmartly,
+						{
+							context: mockContext,
+							attributes: attrs,
+							overrides,
+						},
+					],
+				],
+			},
+		});
 
-        expect(SDK).not.toHaveBeenCalled();
-        expect(mockCreateContext).not.toHaveBeenCalled();
-        expect(mockCreateContextWith).not.toHaveBeenCalled();
+		expect(SDK).not.toHaveBeenCalled();
+		expect(mockCreateContext).not.toHaveBeenCalled();
+		expect(mockCreateContextWith).not.toHaveBeenCalled();
 
-        expect(mockContext.attributes).toHaveBeenCalledTimes(1);
-        expect(mockContext.attributes).toHaveBeenCalledWith(attrs);
+		expect(mockContext.attributes).toHaveBeenCalledTimes(1);
+		expect(mockContext.attributes).toHaveBeenCalledWith(attrs);
 
-        expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledTimes(1);
-        expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledWith(overrides);
+		expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledTimes(1);
+		expect(wrapper.vm.$absmartly.overrides).toHaveBeenCalledWith(overrides);
 
-        expect(wrapper.vm.$absmartly).toBe(mockContext);
+		expect(wrapper.vm.$absmartly).toBe(mockContext);
 
-        done();
-    });
+		done();
+	});
 
-    it("should add global $absmartly context object", (done) => {
-        const wrapper = mount(Component, {
-            global: {
-                plugins: [
-                    [
-                        ABSmartly,
-                        {
-                            sdkOptions,
-                        },
-                    ],
-                ],
-            },
-        });
+	it("should add global $absmartly context object", (done) => {
+		const wrapper = mount(Component, {
+			global: {
+				plugins: [
+					[
+						ABSmartly,
+						{
+							sdkOptions,
+						},
+					],
+				],
+			},
+		});
 
-        expect(wrapper.vm.$absmartly).toBeInstanceOf(Context);
+		expect(wrapper.vm.$absmartly).toBeInstanceOf(Context);
 
-        done();
-    });
+		done();
+	});
 
-    it("should add options.globalName context object", (done) => {
-        const wrapper = mount(Component, {
-            global: {
-                plugins: [
-                    [
-                        ABSmartly,
-                        {
-                            sdkOptions,
-                            globalName: "$exp",
-                        },
-                    ],
-                ],
-            },
-        });
+	it("should add options.globalName context object", (done) => {
+		const wrapper = mount(Component, {
+			global: {
+				plugins: [
+					[
+						ABSmartly,
+						{
+							sdkOptions,
+							globalName: "$exp",
+						},
+					],
+				],
+			},
+		});
 
-        expect(wrapper.vm.$exp).toBeInstanceOf(Context);
+		expect(wrapper.vm.$exp).toBeInstanceOf(Context);
 
-        done();
-    });
+		done();
+	});
 
-    it("should register components by default", (done) => {
-        const wrapper = mount(Component, {
-            global: {
-                plugins: [
-                    [
-                        ABSmartly,
-                        {
-                            sdkOptions,
-                        },
-                    ],
-                ],
-            },
-        });
+	it("should register components by default", (done) => {
+		const wrapper = mount(Component, {
+			global: {
+				plugins: [
+					[
+						ABSmartly,
+						{
+							sdkOptions,
+						},
+					],
+				],
+			},
+		});
 
-        const expectedComponents = {
-            Treatment,
-        };
+		const expectedComponents = {
+			Treatment,
+		};
 
-        for (const componentName of Object.keys(expectedComponents)) {
-            expect(wrapper.__app._context.components).toHaveProperty(componentName);
-        }
+		for (const componentName of Object.keys(expectedComponents)) {
+			expect(wrapper.__app._context.components).toHaveProperty(componentName);
+		}
 
-        done();
-    });
+		done();
+	});
 
-    it("should not register components when options.globalComponents is false", (done) => {
-        const globalComponents = false;
-        const wrapper = mount(Component, {
-            global: {
-                plugins: [
-                    [
-                        ABSmartly,
-                        {
-                            sdkOptions,
-                            globalComponents,
-                        },
-                    ],
-                ],
-            },
-        });
+	it("should not register components when options.globalComponents is false", (done) => {
+		const globalComponents = false;
+		const wrapper = mount(Component, {
+			global: {
+				plugins: [
+					[
+						ABSmartly,
+						{
+							sdkOptions,
+							globalComponents,
+						},
+					],
+				],
+			},
+		});
 
-        const expectedComponents = {
-            Treatment,
-        };
+		const expectedComponents = {
+			Treatment,
+		};
 
-        for (const componentName of Object.keys(expectedComponents)) {
-            expect(wrapper.__app._context.components).not.toHaveProperty(componentName);
-        }
+		for (const componentName of Object.keys(expectedComponents)) {
+			expect(wrapper.__app._context.components).not.toHaveProperty(componentName);
+		}
 
-        done();
-    });
+		done();
+	});
 });
