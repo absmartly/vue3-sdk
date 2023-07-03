905d39a30a9d153ae63f7d505ba8782e8af565fb
Giorgi Tabatadze <android.giorgi@gmail.com>
2023-07-03 19:50:02 +0400

    Add demo

---
+++ examples/demo/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {inject} from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const $absmartly = inject('$absmartly');
+$absmartly.track("payment", {
+	amount: Math.floor(Math.random * 10000)
+});
+
+</script>
+
+<template>
+	<header>
+		<div class="wrapper">
+			<treatment name="multiple owners">
+				<template #0>
+					<HelloWorld msg="You did it! #0"/>
+				</template>
+				<template #1="{ config }">
+					<HelloWorld msg="You did it! #1"/>
+				</template>
+				<template #loading>
+					Please wait
+				</template>
+			</treatment>
+		</div>
+	</header>
+</template>
+
+<style scoped>
+header {
+	line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+	header {
+		display: flex;
+		place-items: center;
+		padding-right: calc(var(--section-gap) / 2);
+	}
+
+	header .wrapper {
+		display: flex;
+		place-items: flex-start;
+		flex-wrap: wrap;
+	}
+}
+</style>
---
+++ examples/demo/src/main.js
@@ -0,0 +1,24 @@
+import './assets/main.css'
+import absmartly from '@absmartly/vue3-sdk';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App)
+  .use(absmartly.ABSmartlyVue, {
+    sdkOptions: {
+      endpoint: import.meta.env.VITE_ABSMARTLY_ENDPOINT,
+      apiKey: import.meta.env.VITE_ABSMARTLY_API_KEY,
+      environment: "prod",
+      application: "web",
+    },
+    context: {
+      units: {
+        user_id: Math.floor(Math.random * 100000)
+      }
+    },
+    attributes: {
+      user_agent: navigator.userAgent
+    }
+  })
+  .mount('#app')
